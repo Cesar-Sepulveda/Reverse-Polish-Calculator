8d91b92d1c4e217db1ea98b071666e5e53dae651
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-02-20 12:13:23 -0800

    Update main.c

--- main.c
+++ main.c
@@ -17,20 +17,33 @@ double val[MAXVAL];
 char buf[BUFSIZE];
 int bufp = 0;
 
-int strindex(const char * s, const char* t) {
-	char * u = strstr(s, t);
-	return u == NULL ? -1 : u - s;
-}
+#define FUNC 'n'
 
-int strrindex(const char * s, const char* t) { //Exercise 4.1
-	char* prev = NULL;
-	char * u = s;
-	while ((u = strstr(u, t)) != NULL) {
-		prev = u;
-		++u;
+//Exercise 4.2
+//----------------------------------------------------
+double atof_(char s[]) {	
+	double val, power, sci;
+	int i, sign;
+	for (i = 0; isspace(s[i]); i++);
+	sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-') { i++; }
+	for (val = 0.0; isdigit(s[i]); i++) {
+		val = 10.0 * val + (s[i] - '0');
 	}
-	return prev == NULL ? -1 : prev - s;
+	if (s[i] == '.') { i++; }
+	for (power = 1.0; isdigit(s[i]); i++) {
+		val = 10.0 * val + (s[i] - '0');
+		power *= 10;
+	}
+	if (s[i] == 'e' || s[i] == 'E') { ++i; }
+	for (sci = 1.0; isdigit(s[i]); ++i) {
+		val = 10.0 * val + (s[i] - '0');
+		sci *= 10;
+	}
+
+	return sign * val / power * sci;
 }
+//------------------------------------------------------
 
 void push(double f) {
 	if (sp < MAXVAL) {
@@ -41,20 +54,70 @@ void push(double f) {
 	}
 }
 
-double pop(void) { 
+double pop(void) {
 	if (sp > 0) {
 		return val[--sp];
 	}
-	else { 
-		printf("error: stack empty\n");           
-		return 0.0; } 
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
 }
 
-int getch(void) { 
-	return (bufp > 0) ? buf[--bufp] : getchar(); 
+//Exercise 4.4
+//-----------------------------------------------------------
+void print_top(){
+	double print = val[sp];
+	printf("%g", print);
 }
 
-void ungetch(int c) { 
+void clear() {
+	sp = 0;
+}
+
+void swap_stack() {
+	double val1, val2;
+	val1 = pop();
+	val2 = pop();
+	push(val1);
+	push(val2);
+}
+
+void duplicate_top() {
+	double top = pop();
+	push(top);
+	push(top);
+}
+//-----------------------------------------------------------
+
+void operation(char s[]) {
+	double op;
+	if (strcmp(s, "sin") == 0) {
+		push(sin(pop()));
+	}
+	else if (strcmp(s, "cos") == 0) {
+		push(cos(pop()));
+	}
+	else if (strcmp(s, "tan") == 0) {
+		push(tan(pop()));
+	}
+	else if (strcmp(s, "exp") == 0) {
+		push(exp(pop()));
+	}
+	else if (strcmp(s, "pow") == 0) {
+		op = pop();
+		push(pow(pop(), op));
+	}
+	else {
+		printf("error: invalid operation\n");
+	}
+}
+
+int getch(void) {
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
 	if (bufp >= BUFSIZE) {
 		printf("ungetch: too many characters\n");
 	}
@@ -75,6 +138,19 @@ int getop(char s[]) {
 	if (c == '.') {
 		while (isdigit(s[++i] = c = getch()));
 	}
+	if (islower(c)) {
+		while (islower(s[++i] = c = getch()));
+		if (c != EOF) {
+			ungetch(c);
+		}
+		s[i] = '\0';
+		if (strlen(s) > 1) {
+			return FUNC;
+		}
+		else {
+			return c;
+		}
+	}
 	s[i] = '\0';
 	if (c != EOF) { ungetch(c); }
 	return NUMBER;
@@ -85,40 +161,68 @@ int getop(char s[]) {
 
 
 int main() {
-	int type;       
-	double op2;       
+	int type;
+	double op2;
 	char s[MAXOP];
+	printf("RNP Calculator:\n");
+	double var_array[26];
 	while ((type = getop(s)) != EOF) {
-		switch (type) { 
-		case NUMBER:               
-			push(atof(s));               
-			break;           
-		case '+':               
-			push(pop() + pop());               
-			break;           
-		case '*':               
-			push(pop() * pop());               
-			break;           
-		case '-':               
-			op2 = pop();               
-			push(pop() - op2);               
-			break;           
-		case '/':               
-			op2 = pop();               
+		switch (type) {
+		case NUMBER:
+			push(atof(s));
+			break;
+		//Exercise 4.5
+		//-------------------------------------------------------------
+		case FUNC:
+			operation(s);
+			break;
+		//-------------------------------------------------------------
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+		case '/':
+			op2 = pop();
 			if (op2 != 0.0) {
 				push(pop() / op2);
 			}
 			else {
 				printf("error: zero divisor\n");
 			}
-			break;           
-		case '\n':               
-			printf("\t%.8g\n", pop());               
-			break;           
-		default:               
-			printf("error: unknown command %s\n", s);               
+			break;
+			//Exercise 4.3
+			//-------------------------------------------------------------
+		case '%':	
+			op2 = pop();
+			if (op2 != 0.0) {
+				push(fmod(pop(), op2));
+			}
+			else {
+				printf("error: zero modulus\n");
+			}
+			break;
+			//-------------------------------------------------------------
+		case '\n':
+			printf("\t%.8g\n", pop());
+			break;
+		default:
+			//Exercise 4.6
+			//-------------------------------------------------------------
+			if (type <= 'Z' && type >= 'A') {
+				push(var_array[type - 'A']);
+			}
+			//-------------------------------------------------------------
+			else {
+				printf("error: unknown command %s\n", s);
+			}
 			break;
 		}
-	}       
+	}
 	return 0;
 }
